b7717b99c4b736515b8b5e33c44cd6c994af2cd6
razyal8 <razyal8@gmail.com>
2023-10-06 16:43:23 +0300

    Fix tsconfig and route register app

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,12 @@
-import fastify, { FastifyInstance } from 'fastify';
+import fastify, { type FastifyInstance } from 'fastify';
+import registerAllRoutes from '../routes/index';
 
 const app: FastifyInstance = fastify({});
 
 app.ready((err: Error) => {
   if (err) throw err;
 });
+
+registerAllRoutes(app);
 
 export default app;
